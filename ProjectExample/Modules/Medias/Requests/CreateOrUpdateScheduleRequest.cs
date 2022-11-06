db29989e00621878cf5d655cdffc9be89779cd43
amchanh1611 <am.chanh16111@gmail.com>
2022-11-06 13:31:08 +0700

    CompleteFluenValidateSchedule

--- ProjectExample/Modules/Medias/Requests/CreateOrUpdateScheduleRequest.cs
+++ ProjectExample/Modules/Medias/Requests/CreateOrUpdateScheduleRequest.cs
@@ -22,19 +22,33 @@ namespace ProjectExample.Modules.Medias.Requests
             this.repository = repository;
 
             //FluentValidation
-            RuleFor(x => x.DateStart).NotEmpty().WithMessage("{PropertyName} is required")
-            .Must(CheckDateExist).WithMessage("There is a schedule during this time");
+            RuleFor(x => x.DateStart).NotEmpty().WithMessage("{PropertyName} is required");
 
             RuleFor(x => x.DateEnd).NotEmpty().WithMessage("{PropertyName} is required")
-            .GreaterThan(x=>x.DateStart).WithMessage("{PropertyName} greater than {ComparisonProperty}")
-            .Must(CheckDateExist).WithMessage("There is a schedule during this time");
+            .GreaterThan(x => x.DateStart).WithMessage("{PropertyName} greater than {ComparisonProperty}");
 
-            RuleFor(x => x.TimeStart).NotEmpty().WithMessage("{PropertyName} is required")
-                .Must(CheckTimeExist).WithMessage("There is a schedule during this time");
+            RuleFor(x => x.TimeStart).NotEmpty().WithMessage("{PropertyName} is required");
 
             RuleFor(x => x.TimeEnd).NotEmpty().WithMessage("{PropertyName} is required")
-               .GreaterThan(x => x.TimeStart).WithMessage("{PropertyName} greater than {ComparisonProperty}")
-               .Must(CheckTimeExist).WithMessage("There is a schedule during this time"); 
+               .GreaterThan(x => x.TimeStart).WithMessage("{PropertyName} greater than {ComparisonProperty}");
+
+            RuleFor(x => x).Must((x) =>
+            {
+                List<Schedule> listSchedule = repository.Schedule.FindByCondition(
+               z => (z.DateStart <= x.DateStart && z.DateEnd >= x.DateStart ||
+               z.DateStart <= x.DateEnd && z.DateEnd >= x.DateEnd)).ToList();
+
+                if (listSchedule.Count == 0)
+                    return true;
+
+                foreach (Schedule schedule in listSchedule)
+                {
+                    if (schedule.TimeStart <= x.TimeStart && schedule.TimeEnd >= x.TimeStart ||
+                    schedule.TimeStart <= x.TimeEnd && schedule.TimeEnd >= x.TimeEnd)
+                        return false;
+                }
+                return true;
+            }).WithMessage("There is a schedule during this time");
 
             RuleFor(x => x.MediaId).NotEmpty().WithMessage("{PropertyName} is required")
                 .Must((_, media) => 
@@ -45,24 +59,24 @@ namespace ProjectExample.Modules.Medias.Requests
                     return false;
                 }).WithMessage("Media does not exist");
         }
-        private bool CheckDateExist(DateTime? date)
-        {
-            List<Schedule> listSchedule = repository.Schedule.FindByCondition(
-                x=>(x.DateStart<=date.Value&&x.DateEnd<=date.Value)).ToList();
+        //private bool CheckDateExist(DateTime? date)
+        //{
+        //    List<Schedule> listSchedule = repository.Schedule.FindByCondition(
+        //        x=>(x.DateStart<=date.Value&&x.DateEnd<=date.Value)).ToList();
             
-            return true;
-        }
-        private bool CheckTimeExist(TimeSpan? time)
-        {
-            List<Schedule> listSchedule = repository.Schedule.FindAll().ToList();
-            foreach (var schedule in listSchedule)
-            {
-                if (time > schedule.TimeStart && time < schedule.TimeEnd)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
+        //    return true;
+        //}
+        //private bool CheckTimeExist(TimeSpan? time)
+        //{
+        //    List<Schedule> listSchedule = repository.Schedule.FindAll().ToList();
+        //    foreach (var schedule in listSchedule)
+        //    {
+        //        if (time > schedule.TimeStart && time < schedule.TimeEnd)
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //    return true;
+        //}
     }
 }
